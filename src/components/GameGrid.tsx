79294ad1598686c1e1bf82cb28828cff30018d3f
Harsh Sharma <harshsharma2526@gmail.com>
2023-07-28 17:07:47 +0530

    Fetch Genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -6,7 +6,7 @@ import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
   const { games, error, isLoading } = useGames();
-  const skeletons = [1, 2, 3, 4, 5, 6];
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
     <>
       {error && <Text>{error}</Text>}
