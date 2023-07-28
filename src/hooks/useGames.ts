23938a6b5ee0feb3afe7896eae00601a5c795ae3
Harsh Sharma <harshsharma2526@gmail.com>
2023-07-28 08:41:35 +0530

    Dispalying Platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,10 +2,17 @@ import React, { useEffect, useState } from 'react'
 import apiClient from '../services/api-client';
 import { CanceledError } from 'axios';
 
+export interface Platfrom {
+  id: number;
+  name: string;
+  slug: string;
+}
+
 export interface Game {
     id: number;
     name: string;
     background_image: string;
+    parent_platforms: { platform: Platfrom }[]
   }
   
   interface FetchGameResponse {
